1cafc792419a916f829832f38bc7d35eb7868a94
wcheo <wcheo@cafe24corp.com>
2017-12-29 11:09:04 +0900

    #1 초기 설정

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Mushfit from './Mushfit';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Mushfit/>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Mushfit.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Mushfit extends React.Component {
+    render() {
+        let sText = 'Mushfit';
+
+        return (
+            <div>{sText}</div>
+        );
+    }
+}
+
+export default Mushfit;
