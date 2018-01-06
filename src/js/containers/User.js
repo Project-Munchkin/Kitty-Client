b4d221f244e958579354ec021ac32c25f314def5
haegul <phg2491@gmail.com>
2018-01-06 17:46:36 +0900

    User Page Prototype

--- src/js/containers/User.js
+++ src/js/containers/User.js
@@ -15,6 +15,38 @@ class User extends Component {
         return (
             <div>
                 <Header/>
+                <div className="content">
+                    <div className="content-explain">
+                        <span>
+                            피팅을 원하시는 회원님의 <br/>정보를 알려주세요!
+                        </span>
+                    </div>
+                    <div className="select-box user">
+                        <div className="selector toggle">
+                            <span className="selector-label">성별</span>
+                            <div>남</div>
+                        </div>
+                        <div className="selector click">
+                            <span className="selector-label">체형</span>
+                            <div className="selector-">
+                                <span>통통</span>
+                                <span>보통</span>
+                                <span>마름</span>
+                            </div>
+                        </div>
+                        <div className="selector option">
+                            <span className="selector-label">키</span>
+                            <div>
+                                <option>
+                                    170cm
+                                </option>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <span>다음으로 >></span>
+                    </div>
+                </div>
             </div>
         );
     }
