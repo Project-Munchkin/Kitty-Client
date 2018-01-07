0158c6f872f1cc8a4bbf1220d6b9c2ce506928c9
heowc <heowc1992@gmail.com>
2018-01-07 13:26:48 +0900

    Result 프로트 타입 - 프로토 타입 이미지 추가 - result.css 추가

--- src/js/containers/Result.js
+++ src/js/containers/Result.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 
+import Header from "../components/header";
+
 class Result extends Component {
 
     constructor(props){
@@ -9,7 +11,42 @@ class Result extends Component {
     render() {
         return (
             <div>
-                <h1>result</h1>
+                <Header/>
+                <div className="content">
+                    <div className="content-explain">
+                        <span>
+                            어떤 브랜드를 둘러보실래요?
+                        </span>
+                    </div>
+                    <div className="result">
+                        <ul>
+                            <li>
+                                <div className="img-wrapper">
+                                    <img src="../../img/uniqlo.gif"/>
+                                </div>
+                                <span className="label">유니클로</span>
+                            </li>
+                            <li>
+                                <div className="img-wrapper">
+                                    <img src="../../img/giordano.png"/>
+                                </div>
+                                <span className="label">지오다노</span>
+                            </li>
+                            <li>
+                                <div className="img-wrapper">
+                                    <img src="../../img/zara.png"/>
+                                </div>
+                                <span className="label">자라</span>
+                            </li>
+                            <li>
+                                <div className="img-wrapper">
+                                    <img src="../../img/hm.png"/>
+                                </div>
+                                <span className="label">H&M</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     }
