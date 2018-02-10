e17a28f38235a4fd97f992f3760cc1e7d6c53f98
heowc <heowc1992@gmail.com>
2018-02-10 15:54:24 +0900

    Recommend 수정 - BottomButton 수정
    Resolve: #9

--- src/js/components/buttons/BottomButton.js
+++ src/js/components/buttons/BottomButton.js
@@ -3,7 +3,7 @@ import React from "react";
 export default ({name, onClickListener})=>{
     return (
         <div className="button bottom">
-            <button onClick={onClickListener}>{name} >></button>
+            <button onClick={onClickListener}>{name}</button>
         </div>
     )
 }
