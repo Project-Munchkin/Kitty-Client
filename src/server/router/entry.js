6c9e46c3986e35345130615d859673c961372f33
haegul <phg2491@gmail.com>
2018-02-17 14:02:59 +0900

    mobile pc 분기 처리

--- src/server/router/entry.js
+++ src/server/router/entry.js
@@ -1,10 +1,19 @@
 import express from "express";
+import MobileDetect from "mobile-detect";
 
 const router = express.Router();
 
 router.get('/', (req, res, next)=>{
+    const md = new MobileDetect(req.headers['user-agent']);
+
+    console.log('md.mobile()', md.mobile());
     res.type('.html');
-    res.redirect('/static/mobile/html/index.html');
+    if(md.mobile()){
+        res.redirect('/static/mobile/html/index.html');
+    }
+    else {
+        res.redirect('/static/pc/html/index.html');
+    }
 });
 
 export default router;
