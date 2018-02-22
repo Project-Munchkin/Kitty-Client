47dfa2421ad52757cf653fb3ed596dcba7bc07d5
haegul <phg2491@gmail.com>
2018-02-22 23:50:04 +0900

    axios 연동

--- src/server/router/api.js
+++ src/server/router/api.js
@@ -1,10 +1,16 @@
 import express from "express";
-
+import axios from "axios";
 const router = express.Router();
 
-router.get('/', (req, res, next)=>{
-    res.json({
-        status: 200
+const url = ''; // 여기에 서버 url을 넣으면 됌.
+
+router.post('/result', (req, res, next) => {
+
+    axios.post(url).then((response)=>{
+        res.json({
+            status: 200,
+            result: response.data
+        });
     });
 });
 
